e6270e99ebda5f1d99fb463f8281c3411d023f2c
chestnuts36 <45006585+chestnuts36@users.noreply.github.com>
2022-01-30 18:09:17 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -24,7 +24,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('|#countdown 3||洋||仔|祝|你|新|年|快|乐|mua|#rectangle|');
+      S.UI.simulate('|#countdown 3||洋||仔||祝||你||新||年||快||乐||mua|#rectangle|');
     }
 
     S.Drawing.loop(function () {
